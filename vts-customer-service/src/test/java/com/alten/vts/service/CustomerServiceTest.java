96fe96569d97e0d8a13220f14c12cdef3a4b3d56
ahmed.gawad <ahmed.gawad@ahmeds-mbp>
2019-10-07 01:14:19 +0200

    Kafka fixes.

--- vts-customer-service/src/test/java/com/alten/vts/service/CustomerServiceTest.java
+++ vts-customer-service/src/test/java/com/alten/vts/service/CustomerServiceTest.java
@@ -4,12 +4,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.alten.vts.dto.CustomerDTO;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EmbeddedKafka
 public class CustomerServiceTest {
 	
 	@Autowired
